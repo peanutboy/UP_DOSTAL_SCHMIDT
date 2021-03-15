40dda585a90b43ad5c72568665a6bbed03cf59c4
peanutboy <44230552+peanutboy@users.noreply.github.com>
2021-03-15 17:54:20 +0100

    Initial commit

---
+++ Assets/UP_Dostal-Schmidt/camray.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camray : MonoBehaviour
+{
+    public int maxDistance;
+    public int layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // fixedUpdate  can run once, zero, or several times per frame, 
+    //depending on how many physics frames per second are set in the time settings,
+    //and how fast/slow the framerate is.
+    void Update()
+    {
+        RaycastHit hit;
+        //ray = Vectororigin, out hit = vector direction
+        
+        var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, maxDistance) && hit.rigidbody != null)
+        {
+            //forcemode applies instantforce depending on mass
+            //.AddForce(ray.direction * hitForce)
+            hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+
+        }
+        
+    }
+}
